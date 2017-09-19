11e714898f75cfd50a25b4969d53097195d74e4d
admin <kongtrolink_bsp@163.com>
2017-09-19 15:45:01 +0800

    【修改】:添加百度地图的UI布局 【提交人】:翁高剑

--- app/src/main/java/com/zhaochewei/kian/zhaochewei/MainActivity.java
+++ app/src/main/java/com/zhaochewei/kian/zhaochewei/MainActivity.java
@@ -1,13 +1,231 @@
 package com.zhaochewei.kian.zhaochewei;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.map.OverlayOptions;
+import com.baidu.mapapi.model.LatLng;
+import com.zhaochewei.kian.zhaochewei.service.LocationService;
+
+import java.util.ArrayList;
+
+public class MainActivity extends Activity {
+
+    private static final String TAG = "EasyParking";
+    private LocationService locationService;
+    private final int SDK_PERMISSION_REQUEST = 127;
+    private MapView mMapView = null;
+    private BaiduMap mBaiduMap =null;
+    private String permissionInfo;
+    private ImageView myLocation;
+    private LatLng mPoint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
         setContentView(R.layout.activity_main);
+        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.navigation_bar);
+        initBaiduMapView();
+        getPersimmions();
     }
+
+    @TargetApi(23)
+    private void getPersimmions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ArrayList<String> permissions = new ArrayList<String>();
+            /***
+             * 定位权限为必须权限，用户如果禁止，则每次进入都会申请
+             */
+            // 定位精确位置
+            if(checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+            }
+            if(checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+            }
+			/*
+			 * 读写权限和电话状态权限非必要权限(建议授予)只会申请一次，用户同意或者禁止，只会弹一次
+			 */
+            // 读写权限
+            if (addPermission(permissions, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                permissionInfo += "Manifest.permission.WRITE_EXTERNAL_STORAGE Deny \n";
+            }
+            // 读取电话状态权限
+            if (addPermission(permissions, Manifest.permission.READ_PHONE_STATE)) {
+                permissionInfo += "Manifest.permission.READ_PHONE_STATE Deny \n";
+            }
+
+            if (permissions.size() > 0) {
+                requestPermissions(permissions.toArray(new String[permissions.size()]), SDK_PERMISSION_REQUEST);
+            }
+        }
+    }
+
+    @TargetApi(23)
+    private boolean addPermission(ArrayList<String> permissionsList, String permission) {
+        if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) { // 如果应用没有获得对应权限,则添加到列表中,准备批量申请
+            if (shouldShowRequestPermissionRationale(permission)){
+                return true;
+            }else{
+                permissionsList.add(permission);
+                return false;
+            }
+
+        }else{
+            return true;
+        }
+    }
+
+    @TargetApi(23)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        // TODO Auto-generated method stub
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // -----------location config ------------
+        locationService = ((EasyParkingApplication) getApplication()).locationService;
+        //获取locationservice实例，建议应用中只初始化1个location实例，然后使用，可以参考其他示例的activity，都是通过此种方式获取locationservice实例的
+        locationService.registerListener(mListener);
+        //注册监听
+        int type = getIntent().getIntExtra("from", 0);
+        if (type == 0) {
+            locationService.setLocationOption(locationService.getDefaultLocationClientOption());
+        } else if (type == 1) {
+            locationService.setLocationOption(locationService.getOption());
+        }
+
+        locationService.start();// 定位SDK
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        locationService.unregisterListener(mListener); //注销掉监听
+        locationService.stop(); //停止定位服务
+    }
+
+    /*****
+     *
+     * 定位结果回调，重写onReceiveLocation方法，可以直接拷贝如下代码到自己工程中修改
+     *
+     */
+    private BDLocationListener mListener = new BDLocationListener() {
+
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            // TODO Auto-generated method stub
+            if (null != location && location.getLocType() != BDLocation.TypeServerError) {
+                //Log.d("EasyParking", sb.toString());
+                mPoint = new LatLng(location.getLatitude(),location.getLongitude());
+            }
+        }
+
+    };
+
+    private void setLocationOnMap(LatLng point){
+        //先清除一下图层
+        mBaiduMap.clear();
+//        //构建Marker图标
+//        BitmapDescriptor bitmap = BitmapDescriptorFactory
+//                .fromResource(R.drawable.icon_marka);
+//        //构建MarkerOption，用于在地图上添加Marker
+//        OverlayOptions option = new MarkerOptions()
+//                .position(point)
+//                .icon(bitmap);
+//        //在地图上添加Marker，并显示
+//        mBaiduMap.addOverlay(option);
+
+        MapStatus mMapStatus = new MapStatus.Builder()
+                //要移动的点
+                .target(point)
+                //放大地图到18倍
+                .zoom(18)
+                .build();
+        //定义MapStatusUpdate对象，以便描述地图状态将要发生的变化
+        MapStatusUpdate mMapStatusUpdate = MapStatusUpdateFactory.newMapStatus(mMapStatus);
+        //改变地图状态
+        mBaiduMap.setMapStatus(mMapStatusUpdate);
+    }
+
+    /**
+     * 除去百度地图上的默认控件
+     * */
+    private void initBaiduMapView() {
+        mMapView = (MapView)findViewById(R.id.bmapView);
+        mBaiduMap = mMapView.getMap();
+        myLocation = (ImageView)findViewById(R.id.my_location);
+
+        myLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setLocationOnMap(mPoint);
+            }
+        });
+
+        mBaiduMap.setOnMapStatusChangeListener(new BaiduMap.OnMapStatusChangeListener(){
+            @Override
+            public void onMapStatusChangeStart(MapStatus arg0) {
+                // 手势操作地图，设置地图状态等操作导致地图状态开始改变。
+            }
+            @Override
+            public void onMapStatusChangeStart(MapStatus var1, int var2){
+
+            }
+            @Override
+            public  void onMapStatusChange(MapStatus var1){
+
+            }
+            @Override
+            public void onMapStatusChangeFinish(MapStatus arg0) {
+                // 地图状态改变结束
+                //target地图操作的中心点。
+                LatLng target = mBaiduMap.getMapStatus().target;
+            }
+        });
+        mMapView.removeViewAt(1);//移除默认百度地图的logo View
+        mMapView.removeViewAt(2);//移除默认百度地图的刻度 View
+        mMapView.showZoomControls(false);//设置不显示缩放控件
+    }
+
 }
